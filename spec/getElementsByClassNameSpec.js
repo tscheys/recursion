30bd4ca93753e2a57c5cfdc298ba277521aab134
Scheysie <tim.scheys@gmail.com>
2015-07-16 10:46:20 +0200

    getElementsByClassName now passes all tests

--- spec/getElementsByClassNameSpec.js
+++ spec/getElementsByClassNameSpec.js
@@ -26,5 +26,4 @@ describe('getElementsByClassName', function(){
     });
     $('body').removeClass('targetClassName');
   });
-
 });
--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,81 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function (className) {
+
+	var body = document.body;
+	var children = body.childNodes;
+	var elements = [];
+	//basecase
+	if(body.classList.contains(className)) {
+		elements.push(body);
+	}
+
+	if(arguments.length === 3) {
+		elements = arguments[2];
+		children = arguments[1].childNodes;
+	}
+
+	_.each(children, function (childNode) {
+		if(childNode.nodeName!== "#text" && childNode.classList.contains(className)) {
+			elements.push(childNode);
+		}
+		if(childNode.childNodes.length > 0) {
+			getElementsByClassName(className, childNode, elements);
+		}
+	});
+
+	return elements;
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+	//Basecase
+	/*if(arguments.length === 1) {
+	var parentNode = document.body;
+	var elements = [];
+}
+	else {
+		var parentNode = arguments[1];
+		var elements = arguments[2];
+	}
+	var childNodes = parentNode.childNodes;
+
+		_.each(childNodes, function (childNode) {
+			var t = childNode;
+			var x = childNode.nodeName;
+			if(x !== "#text") {
+				if(childNode.classList.contains(className))
+				{
+					elements.push(childNode);
+				}
+			}
+			if(childNode.childNodes.length !== 0) {
+			    getElementsByClassName(className, childNode,elements);
+		}
+	});*/
+
+
+
+
+	/*_.each(childNodes, function (childNode) {
+		if(childNode.classList.contains(className))
+		{
+			elements.push(childNode);
+		}
+		if(childNode.childNodes.length !== 0) {
+			getElementsByClassName(className, childNode, elements);*/
+ 
 };
